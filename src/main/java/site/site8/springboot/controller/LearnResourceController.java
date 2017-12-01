ba61c4510af2062edce3d5415cf1668d1bf81baa
yunpeng <yunpeng@iwisedev.com>
2017-12-01 17:59:20 +0800

    添加swagger2

--- src/main/java/site/site8/springboot/controller/LearnResourceController.java
+++ src/main/java/site/site8/springboot/controller/LearnResourceController.java
@@ -2,6 +2,7 @@ package site.site8.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import site.site8.springboot.model.LearnResource;
 import site.site8.springboot.service.LearnResouceService;
@@ -9,16 +10,15 @@ import site.site8.springboot.service.LearnResouceService;
 import java.util.List;
 
 @RestController
+
 public class LearnResourceController {
     @Autowired
     LearnResouceService learnResouceService;
-
-    @RequestMapping("/queryLearnResouceById")
+    @RequestMapping(value = "/queryLearnResouceById",method = RequestMethod.GET)
     public LearnResource queryLearnResouceById(long id){
         return  learnResouceService.queryLearnResouceById(id);
     }
-
-    @RequestMapping("/queryLearnResouce")
+    @RequestMapping(value = "/queryLearnResouce",method = RequestMethod.GET)
     public List<LearnResource> queryLearnResouce(int pageNum, int pageSize){
         return  learnResouceService.queryLearnResouce(pageNum,pageSize);
     }
